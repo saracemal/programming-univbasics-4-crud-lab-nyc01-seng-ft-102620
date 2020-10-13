e946e4312a89dd9a37d2fa5cae1be6f76aad6a9a
Sara Cemal <saracemal@gmail.com>
2020-10-13 04:15:37 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,17 +27,18 @@ end
 
 def retrieve_element_from_index(array, index_number)
   array = ["wow", "I", "am", "really", "learning"]
-  
+  array[2]
 end
 
 def retrieve_first_element_from_array(array)
+  array[0]
   
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array[-1]
 end
 
 def update_element_from_index(array, index_number, element)
-
+  
 end
