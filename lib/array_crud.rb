783c199be4b8872107c5cf51ce2cd301f7bb1c01
Sara Cemal <saracemal@gmail.com>
2020-10-13 04:07:54 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,24 +1,27 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  [2, 4, 6, 8]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array = [2, 4, 6, 8]
+  array.push("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array = [2, 4, 6, 8]
+  array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array.pop("arrays!")
 end
 
 def remove_element_from_start_of_array(array)
+  
   
 end
 
